b78b043c5029289dd5b2e2199c176ba783e0c977
Saber <bjeoui.saber@gmail.com>
2020-11-25 13:19:19 +0100

    initial commit

---
+++ app/src/main/java/saber/bjeoui/mobile_dev_challenge/MainActivity.kt
@@ -0,0 +1,11 @@
+package saber.bjeoui.mobile_dev_challenge
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
