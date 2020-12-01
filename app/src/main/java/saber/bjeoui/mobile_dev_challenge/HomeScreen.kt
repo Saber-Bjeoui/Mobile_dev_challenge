9402fa1af7357e5ba3f366d24d6009f55a7b4f55
Saber <bjeoui.saber@gmail.com>
2020-12-01 18:25:28 +0100

    fixing date format dispaly in home screen

--- app/src/main/java/saber/bjeoui/mobile_dev_challenge/HomeScreen.kt
+++ app/src/main/java/saber/bjeoui/mobile_dev_challenge/HomeScreen.kt
@@ -1,11 +1,24 @@
 package saber.bjeoui.mobile_dev_challenge
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+
 
 class HomeScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_screen)
+        println("hello world")
+
+        val currentDateText: TextView = findViewById(R.id.current_date_text)
+
+        val parser = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss")
+        val formatter = SimpleDateFormat("MMM dd ','yyyy")
+        val parsedDate: String = formatter.format(parser.parse(LocalDateTime.now().toString()))
+        currentDateText.text = parsedDate
+
     }
 }
--- app/src/main/java/saber/bjeoui/mobile_dev_challenge/MainActivity.kt
+++ app/src/main/java/saber/bjeoui/mobile_dev_challenge/MainActivity.kt
@@ -1,11 +1,21 @@
 package saber.bjeoui.mobile_dev_challenge
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        println("hello world")
+
+        val buttonOne = findViewById<Button>(R.id.btn1)
+        buttonOne.setOnClickListener {
+            val intent = Intent(this, HomeScreen::class.java)
+            startActivity(intent);
+        }
     }
 }
